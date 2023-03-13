8f691f8a19dfc93c495fefcace158a79cf5c993c
Nilessh_boii <127608202+IT22093264@users.noreply.github.com>
2023-03-13 09:07:55 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -10,10 +10,26 @@ sum = 1+2+3+....+ 99+100 = 5050
 
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
+  
+  #include <stdio.h>
 
-#include <stdio.h>
+
+// function main begin program execution
 int main() {
+  int end_Point, i, sum = 0;
+
+  printf( "Enter the ending number :" ); // prompt
+  scanf( "%d", &end_Point ); //read the ending number
+
+  for( i = 1; i <= end_Point; ++i){
+    sum = sum + i; // adding numbers
+
+  }
+  printf( "The total is :%d", sum ); // print the total
+
   
   return 0;
-}
+}// end of the main program
+  
+  
 
