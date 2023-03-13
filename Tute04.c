f4f72ef8c7e55c3b589c6f3667ffd3443cc4a7be
Nilessh_boii <127608202+IT22093264@users.noreply.github.com>
2023-03-13 09:17:58 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -6,14 +6,43 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
+int minimum( int no1, int no2 );// declaring function
+int maximum( int no1, int no2 );// declaring function
+int multiply( int no1, int no2 );// declaring function
+
+
+// function main begin program execution
 int main() {
    int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+   printf("Enter a value for no 1 : "); // prompt
+   scanf("%d", &no1); // read an integer
+   printf("Enter a value for no 2 : "); // prompt
+   scanf("%d", &no2); //read an integer
+   printf("%d ", minimum(no1, no2)); // calling function
+   printf("%d ", maximum(no1, no2)); // calling function
+   printf("%d ", multiply(no1, no2)); // calling function
    return 0;
-}
+}// end of the main function
+int minimum( int no1, int no2 ){ // function implementation
+   if( no1 < no2 ){
+      return no1; // return value
+   }
+   else{
+      return no2; // return value
+   }
+} // end of the functio
+
+int maximum( int no1, int no2 ){ // function implementation
+   if( no1 > no2 ){
+      return no1; // return value
+   }
+   else{
+      return no2; // return value
+   }
+}// end of the function
+int multiply( int no1, int no2 ){// function implementation
+   return no1 * no2; // return value
+} // end of the function
+
+
+
