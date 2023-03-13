aebc727b182315e54f2e581336ae6987b3997c2a
Nilessh_boii <127608202+IT22093264@users.noreply.github.com>
2023-03-13 08:38:54 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -18,7 +18,25 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
 
+
+//function main begin program execution
 int main() {
   
+  float distance, payment;
+  
+  printf( "Enter the distance  the van has travelled in killometers :" ); // prompt
+  scanf( "%f", &distance ); // reading the distance
+  
+  if( distance <= 30 ){ // condition checking
+    payment = distance * 50; // calculation
+  }
+  else{ // check condition
+    payment = 30 * 50 + (distance - 30) * 40; // calculation
+  }
+  
+  printf( "The total amount need to pay is :%.2f", payment ); // print the final payment
+
+  
+  
   return 0;
-}
+}// end of the main function
